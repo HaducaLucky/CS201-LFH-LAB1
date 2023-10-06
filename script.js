196e0af35e23426cdf63c96ce1a0bdb678870d9b
linkx16 <147017329+linkx16@users.noreply.github.com>
2023-10-06 11:00:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// MY FIRST FAVOTITE NUMBER
+let firstFavNum = 16;
+// MY SECOND FAVORITE NUMBER
+let secondFavNum = 6;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
